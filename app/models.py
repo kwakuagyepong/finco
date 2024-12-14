070674974ecbda2710018187045c1a9f1bebf07b
Nana Agyepong <nkagyepong1@gmail.com>
2024-12-14 17:40:50 +0000

    update on transaction API

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Authentication:
         try:
             cursor = mysql.connection.cursor()
             cursor.execute("INSERT INTO users (email, password) VALUES (%s, %s)",
-                           (email, password,))
+                           (email, password))
             mysql.connection.commit()
             cursor.close()
             return {'email': email, 'password': password} 
@@ -21,10 +21,24 @@ class Authentication:
     def get_a_user(email, password):
         with mysql.connection.cursor() as cursor:
             cursor.execute("""
-                 SELECT credentials.credencials_id, credentials.role, users_of_credit_union.email, users_of_credit_union.credit_union_id, users_of_credit_union.first_name, users_of_credit_union.status FROM credentials INNER JOIN users_of_credit_union ON credentials.users_id = users_of_credit_union.credit_union_user_id WHERE users_of_credit_union.email = %s AND credentials.password = %s ORDER BY credentials.credencials_id DESC
+                 SELECT credentials.credencials_id, credentials.role, credentials.Users_ID, users_of_credit_union.email, users_of_credit_union.credit_union_id, users_of_credit_union.first_name, users_of_credit_union.status FROM credentials INNER JOIN users_of_credit_union ON credentials.users_id = users_of_credit_union.credit_union_user_id WHERE users_of_credit_union.email = %s AND credentials.password = %s ORDER BY credentials.credencials_id DESC
             """, (email, password))
             user = cursor.fetchone()
         return user 
+
+# Deposit Method
+class CreditUnion_deposit:
+    def push_transaction_desopit(first_name, last_name, transaction_type, amount, account_number, customer_id_number, customer_id_image, credit_union_destination_id, credit_union_originating_id, teller_name_id, date):
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute("INSERT INTO transactions (CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, TRANSACTION_TYPE, AMOUNT, ACCOUNT_NUMBER, CUSTOMER_ID, CUSTOMER_ID_CARD_IMAGE, CREDIT_UNION_DESTINATION_ID, CREDIT_UNION_ORIGINATING_ID, TELLER_NAME, DATE) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                           (first_name, last_name, transaction_type, amount, account_number, customer_id_number, customer_id_image, credit_union_destination_id, credit_union_originating_id, teller_name_id, date))
+            mysql.connection.commit()
+            cursor.close()
+            return {'Customer Name': first_name}
+        except Exception as e:
+            print(e)
+            return None
 
 
 class CreditUnionmodel:
