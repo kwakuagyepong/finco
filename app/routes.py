3ce9e66573573a175c2cb26a0855d89275959558
Nana Agyepong <nkagyepong1@gmail.com>
2025-01-20 16:10:58 +0000

    Error handling

--- app/routes.py
+++ app/routes.py
@@ -157,14 +157,10 @@ def get_approve_transaction():
     if user_role == role_assigned:
 
             user_id_session = session['user_id']
-            
             # credit_id = session['credit_union_id']
-
             required_fields = ['transaction_ID']
             required_fields = ['CREDIT_UNION_ORIGINATING_ID']
-
-            
-             
+           
             data = request.json
             missing_fields = [field for field in required_fields if field not in data]
 
@@ -172,28 +168,39 @@ def get_approve_transaction():
                 error_message = f"Missing fields: {', '.join(missing_fields)}"
                 return jsonify({'error': error_message, 'status_code': 400}), 400
             
+            ORIGINATING_MANAGER_ID = ['ORIGINATING_MANAGER_ID']
+            DESTINATION_MANAGER_ID = ['DESTINATION_MANAGER_ID']
+            
             transaction_ID = data['transaction_ID']
             CREDIT_UNION_ORIGINATING_ID = data['CREDIT_UNION_ORIGINATING_ID']
             
             user_result = users_of_credit_union.get_users_of_credit_union(user_id_session)
             print("Main Result", user_result)
             if user_result:
-                credit_union_id = user_result('credit_union_id')
+                credit_union_id = user_result[5]
+
                 print("credit_union_id", credit_union_id)
                 if credit_union_id == CREDIT_UNION_ORIGINATING_ID:
-                    updated_transaction = update_transaction.get_update_transaction(user_id_session,transaction_ID)
-                    print("Result1", updated_transaction)
-                    if updated_transaction:
-                        return jsonify({'message': 'Transaction Approved', 'status_code': 200}), 200
+                    if not ORIGINATING_MANAGER_ID:
+                        updated_transaction = update_transaction.get_update_transaction(user_id_session,transaction_ID)
+                        print("Result1", updated_transaction)
+                        if updated_transaction:
+                            return jsonify({'message': 'Transaction Approved', 'status_code': 200}), 200
+                        else:
+                            return jsonify({'error': 'Failed to Approve', 'status_code': 500}), 500
                     else:
-                        return jsonify({'error': 'Failed to Approve', 'status_code': 500}), 500
-                else:                                                                                                                       
-                    updated_transaction1 = update_transaction.get_update_transaction_destination_manager(user_id_session,transaction_ID)
-                    print("Result2", updated_transaction1)
-                    if updated_transaction1:
-                        return jsonify({'message': 'Transaction Approved', 'status_code': 200}), 200
-                    else:
-                        return jsonify({'error': 'Failed to Approve', 'status_code': 500}), 500
+                        return jsonify({'error': 'Transaction has already been approved', 'status_code': 500}), 500
+                else:
+                    if not DESTINATION_MANAGER_ID:                                                                                                                           
+                        updated_transaction1 = update_transaction.get_update_transaction_destination_manager(user_id_session,transaction_ID)
+                        print("Result2", updated_transaction1)
+                        if updated_transaction1:
+                            return jsonify({'message': 'Transaction Approved', 'status_code': 200}), 200
+                        else:
+                            return jsonify({'error': 'Failed to Approve', 'status_code': 500}), 500
+                    else: 
+                        return jsonify({'error': 'Transaction has already been approved', 'status_code': 500}), 500
+                        
             return jsonify({'error': 'Result not found', 'status_code': 500}), 500
 
     return jsonify({'error': 'User is not a manager', 'status_code': 404}), 404
