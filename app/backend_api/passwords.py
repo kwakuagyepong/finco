d89acf2163096b8b19b822e211036a910f22e1fc
Joseph Gbekley <gbekleyj@gmail.com>
2025-02-04 14:01:23 +0000

    password v2

--- app/backend_api/passwords.py
+++ app/backend_api/passwords.py
@@ -15,26 +15,27 @@ def assign_password():
             return jsonify({'error': error_message, 'status_code': 400}), 400
         
         user_id = data['user_id']
+        # check of there is data in the credencials_id.
+        try:
+            credentials_id = data['credencials_id']
+        except KeyError:
+            credentials_id = None  # Assign.
         users_password = "Ghana"
         role_assigning = "teller"
-        # credentials_id = data['credencials_id']
-        
- # check of there is data in the credencials_id.
+ 
         if 'credencials_id' in data and data['credencials_id']:
             credentials_id = data['credencials_id']
-            print("before encription", credentials_id)
             result_password = AuthenticationController.change_user_password(credentials_id,users_password)
         else:
             credentials_id = None  # or some default value
             result_password = AuthenticationController.get_user_password(user_id,role_assigning,users_password) 
-        print("This is the Cedentials id", credentials_id)        
+
         if result_password:
             return jsonify({'message': 'Password successfully updated', 'status_code': 200}), 200
         else: 
             return jsonify({'error': 'Failed to process password', 'status_code': 500}), 500
         
     return jsonify({'error': 'Unauthorized user', 'status_code': 400}), 400
-
 
 
 def update_password():
