9605ff26612aa8f7ba1fec59cfd959aa6ecf230a
Nana Agyepong <nkagyepong1@gmail.com>
2025-02-27 10:07:29 +0000

    admin transaction bug fixed

--- app/backend_api/admin_functions.py
+++ app/backend_api/admin_functions.py
@@ -60,9 +60,9 @@ def get_credit_union_transactions():
                         'TELLER':row[10],
                         'ORIGINATING_MANAGER_ID': row[11],
                         'DESTINATION_MANAGER_ID': row[12],
-                        'TIMESTAMP': row[13]
-                        # 'TIMESTAMP': row[13],
-                        # 'STATUS' : row[17]
+                        'TIMESTAMP': row[13],
+                        # 'TIMESTAMP': row[14],
+                        'STATUS' : row[15]
                     },
                     'status_code': 200
                 }
